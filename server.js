d8e3f241bdbc77971cf3c346a26aae2117323323
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2020-01-18 14:29:53 +0100

    refactor render functions

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello SoftJurn!'));
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
